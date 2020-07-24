73d90b90bcc79b55fb15516c93f5169dae487f32
monkey999por <yururi1122@gmail.com>
2020-07-25 01:46:42 +0900

    不要なコードを削除、最初のシリアライズオブジェクトの読み込みのベースをJsonに変更

--- Git-Complete/src/Start.cs
+++ Git-Complete/src/Start.cs
@@ -21,53 +21,22 @@ namespace Git_Complete
             ECommandKeyList<ECommandKeyScrape> eCommandKeyScrapeList = new ECommandKeyList<ECommandKeyScrape>
             {
                 //gitコマンドとオプションのリストを生成する。
-                Value = FileCommon.GetInstanceFromXml<List<ECommandKeyScrape>>(PathProps.HELP_SCRAPE_XML_Path)
+                Value = FileCommon.GetInstanceFromJson<List<ECommandKeyScrape>>(PathProps.HELP_SCRAPE_JSON_Path),
             };
-
-            //読み込み後にファイルにおかしなもしコードの文字が付加されるため、再書き込み
-            FileCommon.OutFileToXml<List<ECommandKeyScrape>>(eCommandKeyScrapeList.Value, PathProps.HELP_SCRAPE_XML_Path);
 
             //test -> コマンド数は136個
             if (eCommandKeyScrapeList.Value.Count != CommonProps.ALL_COMMAND_COUNT)
                 throw new Exception("コマンドの数があってない");
 
-            //jsonのシリアライズ・デシリアライズのテスト
-            if (DebugProps.DEBUG_MODE)
-            {
-                FileCommon.OutFileToJson<List<ECommandKeyScrape>>(eCommandKeyScrapeList.Value, PathProps.HELP_SCRAPE_JSON_Path);
-
-                var temp = FileCommon.GetInstanceFromJson<List<ECommandKeyScrape>>(PathProps.HELP_SCRAPE_JSON_Path);
-
-                //読み込み後にファイルにおかしなもしコードの文字が付加されるため、再書き込み
-                FileCommon.OutFileToXml<List<ECommandKeyScrape>>(temp, PathProps.HELP_SCRAPE_XML_Path);
-
-            }
-
-
-            //test -> swap
-            if (DebugProps.DEBUG_MODE)
-            {
-                /*
-                var test = new EGitCommand("add");
-                test.options.Add("nnnnn");
-                test.options.Add("--gggg");
-                test.synopsis.Add("-ghgh");
-
-                eGitCommandList.Swap(test);
-
-                FileCommon.OutFileToXml<List<EGitCommand>>(eGitCommandList.Value, PathProps.HELP_SCRAPE_XML_Path);
-                */
-            }
-
-
+            //test -> xmlに出力して確認
+            FileCommon.OutFileToXml<List<ECommandKeyScrape>>(eCommandKeyScrapeList.Value, PathProps.HELP_SCRAPE_XML_Path);
+            
             //Gitの公式ヘルプサイトからスクレイピングする用
             if (DebugProps.IS_MAKE_ENTITY_FROM_GIT_HELP)
             {
                 eCommandKeyScrapeList.Value = new List<ECommandKeyScrape>();
                 foreach (var command in CommonProps.ALL_COMMAND)
-                {
                     eCommandKeyScrapeList.Value.Add(new ECommandKeyScrape(command));
-                }
 
                 //git-scm.comから、synopsisを取得する（html parserを使用）
                 var synopsisCommonScraper = new SynopsisCommonScraper();
@@ -79,6 +48,9 @@ namespace Git_Complete
 
                 //xml出力
                 FileCommon.OutFileToXml<List<ECommandKeyScrape>>(eCommandKeyScrapeList.Value, PathProps.HELP_SCRAPE_XML_Path);
+
+                //Json出力
+                FileCommon.OutFileToJson<List<ECommandKeyScrape>>(eCommandKeyScrapeList.Value, PathProps.HELP_SCRAPE_JSON_Path);
             }
 
             //個別解析をする
@@ -90,8 +62,10 @@ namespace Git_Complete
             try
             {
                 //test
-                var test = new ECommandKeyList<ECommandKeyScrape>();
-                test.Value = new List<ECommandKeyScrape>(eParsedHelpScrapeList.Value);
+                var test = new ECommandKeyList<ECommandKeyScrape>
+                {
+                    Value = new List<ECommandKeyScrape>(eParsedHelpScrapeList.Value)
+                };
                 test.Value = gitEntityParser.ShapSynopsisCommonAll(eParsedHelpScrapeList.Value);
             }
             catch (ObjectProcessFailureException<ECommandKeyScrape> e)
